07556faae669456f9ce3fd17f38339a3ae32ada0
Hernanstupniki <hernanst7@gmail.com>
2026-01-10 07:02:00 -0300

    Paths updates.

---
+++ scripts/run/run_trips_bronze.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Starting trips OLTP → Bronze ETL"
+
+spark-submit \
+  --packages io.delta:delta-spark_2.12:3.1.0 \
+  --jars /home/hernan/jars/postgresql-42.7.8.jar \
+  src/bronze/trips_oltp_to_bronze.py
+
+echo "Trips Bronze ETL finished successfully"
---
+++ scripts/run/run_trips_silver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Starting trips Bronze → Silver ETL"
+
+spark-submit \
+  --packages io.delta:delta-spark_2.12:3.1.0 \
+  --conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
+  --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog \
+  --jars /home/hernan/jars/postgresql-42.7.8.jar \
+  src/silver/trips_bronze_to_silver.py
+
+echo "Trips Silver ETL finished successfully"
